ac20d9379099d8e1433846f47ca6841ed1e04d2e
MicheleCS <michelesouza-@outlook.com>
2023-07-04 16:30:48 -0300

    Criando CRUD Person

--- src/modules/person/contexts/service.ts
+++ src/modules/person/contexts/service.ts
@@ -1,12 +1,31 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, ConflictException, Injectable, NotFoundException } from "@nestjs/common";
 import { PersonEntity } from "src/shared/entities/person.entity";
 import { PersonRepository } from "src/shared/repositories/person.repository";
 
 @Injectable()
 export class PersonService {
   private repository = new PersonRepository()
+
   async createPerson(data: PersonEntity) {
+    const personExists = await this.repository.existsPerson(data.cpf)
+
+    if(personExists.length) {
+      throw new BadRequestException('Bad Request Exception')
+    }
     const response = await this.repository.createPerson(data)
     return response
+  }
+
+  async getOnePerson(cpf: string) {
+    const response = await this.repository.getOnePerson(cpf)
+
+    if(response.length === 0) {
+      throw new NotFoundException('Not Found Person')
+    }
+    return response
+  }
+
+  async deletePerson(cpf: string) {
+    return this.repository.deletePerson(cpf)
   }
 }
--- src/shared/repositories/person.repository.ts
+++ src/shared/repositories/person.repository.ts
@@ -2,9 +2,29 @@ import { PersonEntity } from "../entities/person.entity";
 
 export class PersonRepository {
   private person: PersonEntity[] = []
+
   public async createPerson(data: PersonEntity) {
     this.person.push(data)
     
     return data
+  }
+
+  public async existsPerson(cpf: string) {
+    return this.person.filter((person) => person.cpf === cpf)
+  }
+
+  public async getOnePerson(cpf:string) {
+    return await this.person
+    .filter(person => {
+      return person.cpf == cpf
+    })
+  }
+
+  public async deletePerson(cpf: string) {
+    for(let i = 0; i < this.person.length; i++){
+      if(this.person[i].cpf == cpf){
+        this.person.splice(i, 1)
+      }
+    }
   }
 }
